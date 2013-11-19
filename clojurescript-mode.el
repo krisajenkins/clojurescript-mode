fc8d0dfbf6668aaab6894be668ecf61814bdfc60
Kris Jenkins <krisajenkins@gmail.com>
2013-11-19 17:04:13 +0000

    Fixing a clash between clojure-mode and clojurescript-mode mappings.
    M-e in clojure-mode was being overwritten when clojurescript-mode loaded.

--- clojurescript-mode.el
+++ clojurescript-mode.el
@@ -15,24 +15,30 @@
 (require 'clojure-mode)
 
 ;;;###autoload
-(define-derived-mode clojurescript-mode clojure-mode "ClojureScript"
-					 "Major mode for ClojureScript"
-					 (set (make-local-variable 'inferior-lisp-program) "lein cljs-repl")
-					 (evil-define-key 'normal clojurescript-mode-map "\M-e" 'clojurescript-mode/eval-under-point)
-					 (evil-define-key 'insert clojurescript-mode-map "\M-e" 'clojurescript-mode/eval-under-point))
-
-;;;###autoload
 (defun clojurescript-mode/eval-under-point ()
   "Evaluate the form under the point.
 If there is a buffer named 'afterthought', it will execute its contents immediately afterwards."
   (interactive)
   (lisp-eval-defun)
   (let ((afterthought (get-buffer "afterthought")))
-	(when (and afterthought
-			   (not (equal afterthought (current-buffer))))
-	  (save-current-buffer
-		(set-buffer afterthought)
-		(lisp-eval-region (point-min) (point-max))))))
+    (when (and afterthought
+	       (not (equal afterthought (current-buffer))))
+      (save-current-buffer
+	(set-buffer afterthought)
+	(lisp-eval-region (point-min) (point-max))))))
+
+;;;###autoload
+(defvar clojurescript-mode-map
+  (let ((map (make-sparse-keymap)))
+    (evil-define-key 'normal clojurescript-mode-map "\M-e" 'clojurescript-mode/eval-under-point)
+    (evil-define-key 'normal clojurescript-mode-map "\M-e" 'clojurescript-mode/eval-under-point)
+    map)
+  "Keymap for ClojureScript mode.  Inherits from `clojure-mode-map'.")
+
+;;;###autoload
+(define-derived-mode clojurescript-mode clojure-mode "ClojureScript"
+  "Major mode for ClojureScript"
+  (setq-local inferior-lisp-program "lein cljs-repl"))
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist (cons "\\.cljs$" 'clojurescript-mode))
